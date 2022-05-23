e64b3ac0acc7f45dad027906bcbd0ee9f05a6181
Mercury13 <mercury13@ukr.net>
2022-05-23 16:09:47 +0300

    UT

--- MojibakeTest/test_Mojibake.cpp
+++ MojibakeTest/test_Mojibake.cpp
@@ -342,6 +342,30 @@ TEST (CopyS, Utf32Bad)
 }
 
 
+///
+/// Bad UTF-16
+///
+TEST (CopyS, Utf16Bad)
+{
+    std::u16string s;
+    s.push_back('a');
+    s.push_back(0xD900);    // Lone low surrogate
+    s.push_back('b');
+    s.push_back(0xDE00);    // Lone high surrogate
+    s.push_back('c');
+    s.push_back(0xD9AB);    // Double low surrogate
+    s.push_back(0xD9CD);
+    s.push_back('d');
+
+    EXPECT_EQ(8u, s.length());   // Should contain those chars
+
+    char buf[30];
+    auto end = mojibake::copyS(s.begin(), s.end(), buf);
+    std::basic_string_view r (buf, end - buf);
+    EXPECT_EQ("abcd", r);
+}
+
+
 /////
 /////  mojibake::copyM /////////////////////////////////////////////////////////
 /////
@@ -377,6 +401,30 @@ TEST (CopyM, Utf32Bad)
     auto end = mojibake::copyM(s.begin(), s.end(), buf);
     std::basic_string_view r (buf, end - buf);
     EXPECT_EQ("abc" "\xEF\xBF\xBD" "\xD0\x8B" "\xEF\xBF\xBD" "\xE1\x88\xB4" "\xF0\x92\x8D\x85", r);
+}
+
+
+///
+/// Bad UTF-16
+///
+TEST (CopyM, Utf16Bad)
+{
+    std::u16string s;
+    s.push_back('a');
+    s.push_back(0xD900);    // Lone low surrogate
+    s.push_back('b');
+    s.push_back(0xDE00);    // Lone high surrogate
+    s.push_back('c');
+    s.push_back(0xD9AB);    // Double low surrogate
+    s.push_back(0xD9CD);
+    s.push_back('d');
+
+    EXPECT_EQ(8u, s.length());   // Should contain those chars
+
+    char buf[30];
+    auto end = mojibake::copyM(s.begin(), s.end(), buf);
+    std::basic_string_view r (buf, end - buf);
+    EXPECT_EQ("a" "\xEF\xBF\xBD" "b" "\xEF\xBF\xBD" "c" "\xEF\xBF\xBD" "\xEF\xBF\xBD" "d", r);
 }
 
 
